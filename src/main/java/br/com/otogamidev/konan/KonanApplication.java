332d75c4ccaee663e35926faeb48c3380aabe907
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-23 23:09:02 -0300

    begin: Lesson 408/409/410/411 - Starting Konan project

---
+++ src/main/java/br/com/otogamidev/konan/KonanApplication.java
@@ -0,0 +1,13 @@
+package br.com.otogamidev.konan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/otogamidev/konan/TestKonanApplication.java
@@ -0,0 +1,13 @@
+package br.com.otogamidev.konan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKonanApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
