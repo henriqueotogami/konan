eae702938300ecfb48769198622031785c24356f
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-25 11:40:59 -0300

    feature: Lesson 420 - Using parameters in HTTP requests.

--- src/main/java/br/com/otogamidev/konan/controller/ClientManager.java
+++ src/main/java/br/com/otogamidev/konan/controller/ClientManager.java
@@ -2,6 +2,7 @@ package br.com.otogamidev.konan.controller;
 
 import br.com.otogamidev.konan.model.Client;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class ClientManager {
     @GetMapping(path="/whatever")
     public Client getClient() {
         return new Client(28, "Pedro", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Client getClientById(@PathVariable final int id) {
+        return new Client(id, "Maria", "987.654.321-00");
     }
 
 }
