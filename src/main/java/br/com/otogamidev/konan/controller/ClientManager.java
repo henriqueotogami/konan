ee220744f394da75e239082640d4df283f37a17b
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-25 12:36:22 -0300

    feature: Lesson 421 - Using parameters in the URL of the HTTP request.

--- src/main/java/br/com/otogamidev/konan/controller/ClientManager.java
+++ src/main/java/br/com/otogamidev/konan/controller/ClientManager.java
@@ -4,20 +4,43 @@ import br.com.otogamidev.konan.model.Client;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * URL -> http://localhost:8080/clients
+ */
 @RestController
 @RequestMapping(path="/clients")
 public class ClientManager {
 
+    /**
+     * URL -> http://localhost:8080/clients/whatever
+     * @return Retorna um cliente qualquer.
+     */
     @GetMapping(path="/whatever")
     public Client getClient() {
         return new Client(28, "Pedro", "123.456.789-00");
     }
 
+    /**
+     * URL -> http://localhost:8080/clients/2
+     * @param id Identificador único do cliente.
+     * @return Retorna um cliente quando o ID é requisitado no endereço da URL.
+     */
     @GetMapping("/{id}")
-    public Client getClientById(@PathVariable final int id) {
+    public Client getClientByIdInPath(@PathVariable final int id) {
         return new Client(id, "Maria", "987.654.321-00");
+    }
+
+    /**
+     * URL -> http://localhost:8080/clients?id=2
+     * @param id Identificador único do cliente.
+     * @return Retorna um cliente quando o ID é requisitado como parâmetro da URL.
+     */
+    @GetMapping
+    public Client getClientByIdInRequest(@RequestParam(name="id", defaultValue="0") final int id) {
+        return new Client(id, "João Augusto", "111.222.333-44");
     }
 
 }
--- src/main/java/br/com/otogamidev/konan/controller/HttpManager.java
+++ src/main/java/br/com/otogamidev/konan/controller/HttpManager.java
@@ -1,6 +1,12 @@
 package br.com.otogamidev.konan.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/methods")
