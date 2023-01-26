c9110755c94867ebe8d92c46cc77a7ec26c2f7f7
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-26 00:40:58 -0300

    refactor: Lesson 440 - Adding the responsability to update a product to the create new product method in the database.

--- src/main/java/br/com/otogamidev/konan/controller/ProductManager.java
+++ src/main/java/br/com/otogamidev/konan/controller/ProductManager.java
@@ -15,11 +15,11 @@ public class ProductManager {
     @Autowired
     private DataAccessProduct dataAccessProduct;
 
-    @PostMapping
-    public @ResponseBody Product createNewProduct(@Valid final Product newProduct) {
-        System.out.println("ProductManager - createNewProduct(): BEGIN");
+    @RequestMapping(method={RequestMethod.POST, RequestMethod.PUT})
+    public @ResponseBody Product createAndUpdateProduct(@Valid final Product newProduct) {
+        System.out.println("ProductManager - createAndUpdateProduct(): BEGIN");
         dataAccessProduct.save(newProduct);
-        System.out.println("ProductManager - createNewProduct(): END");
+        System.out.println("ProductManager - createAndUpdateProduct(): END");
         return newProduct;
     }
 
@@ -29,9 +29,9 @@ public class ProductManager {
     @GetMapping(path="/{id}")
     public Optional<Product> getProductById(@PathVariable final int id) { return dataAccessProduct.findById(id); }
 
-    @PutMapping
-    public Product putProduct(@Valid final Product product) {
-        dataAccessProduct.save(product);
-        return product;
-    }
+//    @PutMapping
+//    public Product putProduct(@Valid final Product product) {
+//        dataAccessProduct.save(product);
+//        return product;
+//    }
 }
